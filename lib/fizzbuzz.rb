c7251cf5e8e2fb8603ac78324a1c291483340e94
Cosmin <mirceacosmin@gmail.com>
2019-02-22 15:45:47 +0000

    Refactored fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,3 @@
 def fizzbuzz(number)
-  if number % 15 == 0
-    "fizzbuzz"
-  elsif number % 3 == 0
-    "fizz"
-  elsif number % 5 == 0
-    "buzz"
-  end
+  number % 15 == 0 ? "fizzbuzz" : number % 3 == 0 ? "fizz" : "buzz"
 end
